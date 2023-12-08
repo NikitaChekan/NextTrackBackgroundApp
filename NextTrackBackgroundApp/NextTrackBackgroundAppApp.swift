03965d78e56e8f6f6038f073a77b03ec55174f5c
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-12-08 12:31:47 +0300

    Initial Commit

---
+++ NextTrackBackgroundApp/NextTrackBackgroundAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextTrackBackgroundAppApp.swift
+//  NextTrackBackgroundApp
+//
+//  Created by jopootrivatel on 08.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct NextTrackBackgroundAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
